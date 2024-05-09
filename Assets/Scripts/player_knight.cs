b08e8ac47ba891713dd770e420530e169dd5e4f5
SourSushi360 <jplopez365@hotmail.com>
2024-05-08 23:17:13 -0600

    corrected tileset

--- Assets/Scripts/player_knight.cs
+++ Assets/Scripts/player_knight.cs
@@ -4,12 +4,13 @@ using System;
 public partial class player_knight : CharacterBody2D
 {
 	// Stats
-	private const float speed = 200.0f;
+	[Export] public const float speed = 200.0f;
 	[Export] public int hp = 10;
 	// Attributes
-	private Vector2 velocity, direction;
 	public AnimatedSprite2D sprite2D;
-
+	Vector2 direction;
+	// fun
+	
 	// Process
     public override void _Ready()
     {
@@ -17,32 +18,14 @@ public partial class player_knight : CharacterBody2D
     }
     public override void _PhysicsProcess(double delta)
     {
-		velocity = Vector2.Zero;
-		if (Input.IsActionPressed("up")) {
-			velocity.Y -= 1;
-			direction = new Vector2(direction.X, -1);
-		}
-		if (Input.IsActionPressed("down")) {
-			velocity.Y += 1;
-			direction = new Vector2(direction.X, 1);
-		}
-		if (Input.IsActionPressed("left")) {
-			velocity.X -= 1;
-			direction = Vector2.Left;
-		}
-		if (Input.IsActionPressed("right")) {
-			velocity.X += 1;
-			direction = Vector2.Right;
-		}
-		velocity = velocity.Normalized();
-		Velocity = velocity*speed;
-		// velocity.X = Mathf.MoveToward(Velocity.X, 0, 50);
-
+		direction = Input.GetVector("left","right","up","down");
+		Velocity = direction*speed;
+		
 		bool isLeft = direction.X < 0;
 		sprite2D.FlipH = isLeft;
-		if (velocity.X != 0 || velocity.Y != 0) {
-			sprite2D.Animation = "run";
-		} else sprite2D.Animation = "idle";
+		if (Velocity.IsZeroApprox()) {
+			sprite2D.Animation = "idle";
+		} else sprite2D.Animation = "run";
 		MoveAndSlide();
     }
     public override void _Process(double delta)
